95ca69e7d25fee62011b6eacc23b972bd0b8b515
GiovanniDonati <grdonati99@gmail.com>
2026-04-13 20:48:39 -0300

    feat: Using layer Arch, create UserRepository

---
+++ server/src/repositories/user.repository.ts
@@ -0,0 +1,43 @@
+import { prisma } from "../config/prisma";
+import { User } from "../domain/entities/user.entity";
+import { CreateUserDto, UpdateUserDto } from "../domain/dto/user.dto";
+import { Permission, Status } from "../domain/entities/enums";
+
+export class UserRepository {
+  async findAll(): Promise<User[]> {
+    return await prisma.user.findMany() as User[];
+  }
+
+  async findById(id: string): Promise<User | null> {
+    return await prisma.user.findUnique({ where: { id }}) as User | null;
+  }
+
+  async findByName(name: string): Promise<User | null> {
+    return await prisma.user.findFirst({ where: { name }}) as User | null;
+  }
+
+  async create(data: CreateUserDto): Promise<User> {
+    return await prisma.user.create({
+      data: {
+        name: data.name,
+        password: data.password,
+        status: Status.ACTIVE,
+      }
+    }) as User;
+  }
+
+  async update(id: string, data: UpdateUserDto): Promise<User> {
+    return await prisma.user.update({
+      where: { id },
+      data: {
+        name: data.name,
+        password: data.password,
+        status: data.status as Status,
+      }
+    }) as User;
+  }
+
+  async delete(id: string): Promise<void> {
+    await prisma.user.delete({ where: { id } });
+  }
+}

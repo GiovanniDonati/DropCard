3221748d0e02d815ed981ca728a383f3ee8cc3fe
GiovanniDonati <grdonati99@gmail.com>
2026-04-16 21:18:10 -0300

    fix: update table name

--- server/prisma/migrations/20260414155305_v2_tag_table/migration.sql
+++ server/prisma/migrations/20260414155305_v2_tag_table/migration.sql
@@ -1,11 +1,11 @@
 -- CreateTable
-CREATE TABLE "categories" (
+CREATE TABLE "tags" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "tags_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "categories_name_key" ON "categories"("name");
+CREATE UNIQUE INDEX "tags_name_key" ON "tags"("name");

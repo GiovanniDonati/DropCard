c3094aee2b94ed6271c8e1e31c0f469a3a8d8c33
GiovanniDonati <grdonati99@gmail.com>
2026-05-05 20:52:24 -0300

    feat: Started the basic web project, using vitewind

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <div className="w-full h-screen bg-[#040D12] flex flex-col items-center justify-center gap-4 text-[#93B1A6]">
+      <h1 className="text-4xl font-bold">Welcome to Flashcard App</h1>
+      <p className="text-lg">This is a simple application to memorize the studies and concepts, using flashcards.</p>
+    </div>
+  );
+}
+
+export default App;
